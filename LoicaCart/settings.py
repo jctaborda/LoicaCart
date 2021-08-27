5303a85e43374c9ea4c7f5f2e9994ae4617b1994
Julio Taborda <julio_taborda@yahoo.com.ar>
2021-08-27 11:58:52 -0300

    Landing page template

--- LoicaCart/settings.py
+++ LoicaCart/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
-STATICFILES_DIR = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = ['LoicaCart/static',]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
