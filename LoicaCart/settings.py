b9b3666e207e62457f142167979d98fed0591386
Julio Taborda <julio_taborda@yahoo.com.ar>
2021-08-30 22:03:47 -0300

    Display of categories in the menues and created store app with product and product detail templates

--- LoicaCart/settings.py
+++ LoicaCart/settings.py
@@ -50,7 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'category',
-    'accounts'
+    'accounts',
+    'store'
 ]
 
 MIDDLEWARE = [
@@ -76,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'category.context_processors.menu_links'
             ],
         },
     },
--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,4 @@
-"""LoicaCart URL Configuration
-
+"""
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -13,14 +12,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('store/', include('store.urls')),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.store, name='store'),
+    path('<slug:category_slug>/', views.store, name='products_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail')
+]
