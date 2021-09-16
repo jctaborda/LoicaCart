ce27d626946b6864dcbea52c4dd713c9c67f76ba
Julio Taborda <julio_taborda@yahoo.com.ar>
2021-09-16 12:48:30 -0300

    Orders and order review page

--- LoicaCart/settings.py
+++ LoicaCart/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
-    'carts'
+    'carts',
+    'orders'
 ]
 
 MIDDLEWARE = [
--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,4 @@
-"""LoicaCart URL Configuration
-
+"""
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -13,18 +12,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('store/', include('store.urls')),
-    path('cart/', include('carts.urls')),
-    path('orders/', include('orders.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('cart/', include('carts.urls')),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('place_order/', views.place_order, name='place_order'),
+    path('payments/', views.payments, name='payments'),
+
+]
