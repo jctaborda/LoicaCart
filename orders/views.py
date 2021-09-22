a214bd7c7a549b0d2f62794eb9fdff66178b36c3
Julio Taborda <julio_taborda@yahoo.com.ar>
2021-09-22 12:15:01 -0300

    PayPal payment gateway with sandbox account. Move products from cart to order, reduce stock, send order received mail and redirect user to invoice

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,81 @@
-from django.http import HttpResponse
+import json
+
+from django.core.mail import EmailMessage
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+
 from carts.models import CartItem
 from orders.forms import OrderForm
 import datetime
-from orders.models import Order
+from orders.models import Order, Payment, OrderProduct
+import json
+
+from store.models import Product
 
 
 def payments(request):
+    body = json.loads(request.body)
+    order = Order.objects.get(user=request.user, is_ordered=False, order_number=body['orderID'])
+
+    # Save payment info
+    payment = Payment(user=request.user,
+                      payment_id=body['transID'],
+                      payment_method=body['payment_method'],
+                      amount_paid=order.order_total,
+                      status=body['status'],
+                      )
+    payment.save()
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+
+    # Move products from cart to order
+
+    cart_items = CartItem.objects.filter(user=request.user)
+    for item in cart_items:
+        orderproduct = OrderProduct()
+        orderproduct.order_id = order.id
+        orderproduct.payment = payment
+        orderproduct.user_id = request.user.id
+        orderproduct.product_id = item.product_id
+        orderproduct.quantity = item.quantity
+        orderproduct.product_price = item.product.price
+        orderproduct.ordered = True
+        orderproduct.save()
+
+        cart_item = CartItem.objects.get(id=item.id)
+        product_variations = cart_item.variations.all()
+        orderproduct = OrderProduct.objects.get(id=orderproduct.id)
+        orderproduct.variation.set(product_variations)
+        orderproduct.save()
+
+    # Reduce stock of sold products
+        product = Product.objects.get(id=item.product_id)
+        product.stock -= item.quantity
+        product.save()
+
+    # Clear cart items
+    CartItem.objects.filter(user=request.user).delete()
+
+    # Send order received mail to customer
+    mail_subject = 'Thank you for your order!'
+    message = render_to_string('orders/order_received_email.html', {
+        'user': request.user,
+        'order': order,
+    })
+    to_email = request.user.email
+    send_mail = EmailMessage(mail_subject, message, to=[to_email])
+    send_mail.send()
+
+    # Redirect customer to order completed page
+    data = {
+        'order_number': order.order_number,
+        'transID': payment.payment_id,
+    }
+    return JsonResponse(data)
+
+
     return render(request, 'orders/payments.html')
 
 
@@ -60,3 +129,29 @@ def place_order(request, total=0, quantity=0):
             return render(request, 'orders/payments.html', context)
         else:
             return redirect('checkout')
+
+
+def order_complete(request):
+    order_number = request.GET.get('order_number')
+    transID = request.GET.get('payment_id')
+
+    try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_products = OrderProduct.objects.filter(order_id=order.id)
+        payment = Payment.objects.get(payment_id=transID)
+
+        subtotal = 0
+        for i in ordered_products:
+            subtotal += i.product_price * i.quantity
+
+        context = {
+            'order': order,
+            'ordered_products': ordered_products,
+            'order_number': order.order_number,
+            'transID': payment.payment_id,
+            'payment': payment,
+            'subtotal': subtotal,
+        }
+        return render(request, 'orders/order_complete.html', context)
+    except (Payment.DoesNotExist, Order.DoesNotExist):
+        return redirect('home')
